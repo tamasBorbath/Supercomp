9337040a3d0b4289d9a2d9cd731156efe3b1c83d
caeiro-faisal <caeiro_faisal@yahoo.co.in>
2014-11-05 16:05:10 +0100

    preliminary distribution and makefile modification

--- Assignment2/fire-benchmark-A2.1/distribution.c
+++ Assignment2/fire-benchmark-A2.1/distribution.c
@@ -3,10 +3,18 @@
 #include "distribution.h"
 
 
-void parallel_init(int myrank, int nprocs, int nintci, int nintcf, int *local_nintci, int *local_nintcf)
+int min (a,b)
+{
+    if (a>b) { return b;}
+    else {return a;}
+}
+
+
+void parallel_init(int myrank, int nprocs, int nintci, int nintcf, int *local_nintci, int *local_nintcf,double **local_global_index)
 {
 
     int i_quotient, i_remainder;
+    
 	/** Domain Decomposition ***/
 
     i_quotient  = (nintcf-nintci+1) / nprocs;
@@ -14,5 +22,17 @@ void parallel_init(int myrank, int nprocs, int nintci, int nintcf, int *local_ni
 
     *local_nintci = myrank       * i_quotient + min(myrank     , i_remainder) + 1;
     *local_nintcf = (myrank + 1) * i_quotient + min(myrank + 1 , i_remainder);
+
+    if ( ( *local_global_index = (double *) malloc(( *local_nintcf - *local_nintci ) * sizeof(double) ) )
+        == NULL ) 
+    {
+        printf( "malloc failed to allocate distr array" );
+        exit(1);
+    }
+
+    for ( int i = 0; i < *local_nintcf - *local_nintci ; i++ )
+    {
+        *local_global_index[i] = *local_nintci + i;
+    }
 
 }
--- Assignment2/fire-benchmark-A2.1/distribution.h
+++ Assignment2/fire-benchmark-A2.1/distribution.h
@@ -1,7 +1,8 @@
 #ifndef DISTRIBUTION_H_
 #define DISTRIBUTION_H_
 
+#include <stdio.h>
 
-void parallel_init(int myrank, int nprocs, int nintci, int nintcf, int *local_nintci, int *local_nintcf);
+void parallel_init(int myrank, int nprocs, int nintci, int nintcf, int *local_nintci, int *local_nintcf, double **local_global_index);
 
 #endif 
